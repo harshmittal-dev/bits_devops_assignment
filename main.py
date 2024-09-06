9a371b2728df68459d9fbcd2558b88f4c31f74f8
harshmittal-dev <2023mt93394@wilp.bits-pilani.ac.in>
2024-09-06 23:53:31 +0530

    Added print for class

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ class Student:
 
 	def sum(self):
 		return self.m1 + self.m2 + self.m3
+	
+	def __str__(self):
+		print(f"Name: {self.name} Total: {self.sum()}")
 
 if __name__ == "__main__":
 	s = Student("Harsh Mittal", 98,100, 50)
-	print(s.name)
+	print(s)
