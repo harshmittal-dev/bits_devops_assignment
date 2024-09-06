0656bb75d944813ff5a6da100846cf802987b319
Harsh Mittal <harshmittal2210@gmail.com>
2024-09-06 23:37:41 +0530

    Number of subjects increased

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 class Student:
-	def __init__(self, name, m1, m2):
+	def __init__(self, name, m1, m2, m3):
 		self.name = name
 		self.m1 = m1
 		self.m2 = m2
+		self.m3 = m3
 	def sum(self):
-		return self.m1 + self.m2
+		return self.m1 + self.m2 + self.m3
 
 if __name__ == "__main__":
 	s = Student("Harsh Mittal", 98,100)
