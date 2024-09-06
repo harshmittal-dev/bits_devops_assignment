98b072695141044828a44911f4a12980cebbb7f7
Harsh Mittal <harshmittal2210@gmail.com>
2024-09-06 23:21:55 +0530

    Variable Name Correction

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class Student:
 		self.m1 = m1
 		self.m2 = m2
 	def sum(self):
-		return self.m1 + self.m3
+		return self.m1 + self.m2
 
 if __name__ == "__main__":
 	s = Student("Harsh Mittal", 98,100)
